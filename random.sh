0fe7a1271b65952eef9eb21d7b798d7ea0cad48f
windygallery <windyberry@gmail.com>
2024-01-06 00:10:29 +0700

    Change random string to number

--- random.sh
+++ random.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-directory="./ADDtest"    # Specify the directory containing your files
+directory="./test"    # Specify the directory containing your files
 file_type="jpg"      # Specify the file type (e.g., txt, jpg, etc.)
 
 for file in "$directory"/*.$file_type; do
     if [ -f "$file" ]; then
         echo "Processing file: $file"
-        mv "$file" "$directory/$(openssl rand -hex 8).jpg"
+        #mv "$file" "$directory/$(openssl rand -hex 8).jpg"
+        random_number=$(jot -r 1 10000000 99999999)
+        mv "$file" "$directory/$random_number.jpg"
     fi
 done
 
@@ -13,6 +15,8 @@ file_type="JPG"
 for file in "$directory"/*.$file_type; do
     if [ -f "$file" ]; then
         echo "Processing file: $file"
-        mv "$file" "$directory/$(openssl rand -hex 8).jpg"
+        # mv "$file" "$directory/$(openssl rand -hex 8).jpg"
+        random_number=$(jot -r 1 10000000 99999999)
+        mv "$file" "$directory/$random_number.jpg"
     fi
 done
